83f552764bc2682f3840871163dae8513921f7f4
lujanfernaud <lujanfernaud@gmail.com>
2017-12-07 20:28:54 +0000

    Refactor people_controller_spec

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -4,18 +4,38 @@ describe PeopleController do
   describe "#create" do
     context "when person is valid" do
       it "redirects to #show" do
+        person = valid_person
+
         post :create, person: { first_name: "Dong" }
 
-        expect(response).to redirect_to person_path(assigns(:person))
+        expect(response).to redirect_to person
       end
     end
 
     context "when person is invalid" do
       it "renders the 'new' template" do
+        person = invalid_person
+
         post :create, person: { first_name: "" }
 
         expect(response).to render_template :new
       end
     end
+  end
+
+  def valid_person
+    person_double = Person.new
+
+    allow(Person).to receive(:new).and_return(person_double)
+    allow(person_double).to receive(:save).and_return(true)
+
+    person_double
+  end
+
+  def invalid_person
+    person_double = Person.new
+
+    allow(Person).to receive(:new).and_return(person_double)
+    allow(person_double).to receive(:save).and_return(false)
   end
 end
